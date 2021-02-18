b2ae7b9c854bc03a7bf803ef552a9352fe4083d9
Stefan Koch <webokoch@gmx.de>
2021-02-18 18:12:00 +0100

    corrected route for doses#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :new, :show, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
